e5792ec48f3643dd635f7d7a5ed8103f841a480f
Chapizze <chapizze@gmail.com>
2024-12-03 11:18:42 +0100

    WIP Vue 2 => Vue 3 - Fixing comments and header darkmode

--- frontend/src/components/breadcrumb.vue
+++ frontend/src/components/breadcrumb.vue
@@ -1,12 +1,29 @@
 <template>
-    <q-bar class="card-breadcrumb bg-white">
+    <q-bar class="card-breadcrumb" :class="$q.dark.isActive ? 'bg-dark text-white' : 'bg-white text-dark'">
         <template v-if="path">
-            <q-btn :to="path" :label="pathName" no-caps dense flat color="secondary" icon="arrow_back" />
-            <q-separator class="q-mr-sm" vertical inset />
+            <q-btn 
+                :to="path" 
+                :label="pathName" 
+                no-caps 
+                dense 
+                flat 
+                :color="$q.dark.isActive ? 'secondary' : 'primary'" 
+                icon="arrow_back" 
+            />
+            <q-separator 
+                class="q-mr-sm" 
+                vertical 
+                inset 
+                :color="$q.dark.isActive ? 'grey-6' : 'grey-4'"
+            />
         </template>
         <span class="text-bold">{{title}}</span>
         <div v-if="this.$settings?.reviews.enabled && state !== 'EDIT'">
-            <audit-state-icon class="q-mx-sm" :approvals="approvals" :state="state"/>
+            <audit-state-icon 
+                class="q-mx-sm" 
+                :approvals="approvals" 
+                :state="state"
+            />
         </div>
         <q-space />
         <slot name="buttons"></slot>
@@ -22,22 +39,8 @@ export default {
 
     components: {
         AuditStateIcon
-    },
-
-    data: function() {
-        return {
-            
-        }
-    },
-
-    created: function() {
-    },
-
-    methods: {
-        
     }
 }
-
 </script>
 
 <style lang="stylus">
@@ -45,6 +48,7 @@ export default {
     height: 50px
     padding-right: 20px
     font-size: 14px
+    transition: background-color 0.3s, color 0.3s
 }
 
 .card-breadcrumb .q-btn {
